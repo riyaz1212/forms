0728c7a648265c580994a4954f7708368cda4773
riyaz1212 <riyaz.coding@gmail.com>
2023-12-29 21:33:13 -0600

    new update

---
+++ form/src/main/java/com/example/demo/UserEntity.java
@@ -0,0 +1,36 @@
+package com.example.demo;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity // This tells Hibernate to make a table out of this class
+public class UserEntity {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Integer actor_id;
+	private String first_name;
+	private String last_name;
+	public Integer getActor_id() {
+		return actor_id;
+	}
+	public void setActor_id(Integer actor_id) {
+		this.actor_id = actor_id;
+	}
+	public String getFirst_name() {
+		return first_name;
+	}
+	public void setFirst_name(String first_name) {
+		this.first_name = first_name;
+	}
+	public String getLast_name() {
+		return last_name;
+	}
+	public void setLast_name(String last_name) {
+		this.last_name = last_name;
+	}
+	
+
+}
